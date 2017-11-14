c908795f500f2f8699f3c74a8f5c84ade40b783d
Mason <htj889@gmail.com>
2017-11-14 15:44:47 +0900

    스플래쉬 액티비티 적용 및 메인 엑티비티 추가

--- app/src/main/java/hong/mason/fbdatabaseex/SplashActivity.kt
+++ app/src/main/java/hong/mason/fbdatabaseex/SplashActivity.kt
@@ -1,12 +1,32 @@
 package hong.mason.fbdatabaseex
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import java.util.concurrent.TimeUnit
 
 class SplashActivity : AppCompatActivity() {
+    lateinit var disposable : Disposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        disposable = Observable.just(0)
+                .delay(1000, TimeUnit.MILLISECONDS)
+                .subscribe {
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if(!disposable.isDisposed) {
+            disposable.dispose()
+        }
     }
 }
